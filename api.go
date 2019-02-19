38cb363f27f1f7fb9426fbace378a233d4d5afc1
mattdsteele <orphum@gmail.com>
2019-02-18 21:54:40 -0600

    Initial

--- api.go
+++ api.go
@@ -1,16 +1,29 @@
 package main
 import (
 	"fmt"
+	"os"
+
 	"net/http"
 
 	"github.com/peppage/foursquarego"
 )
 
 func main() {
-	fmt.Println("Whoo")
 	httpClient := http.DefaultClient
 	// When creating the client you can specify either clientSecret or the accesstoken
-	client := foursquarego.NewClient(httpClient, "foursquare", "clientId", "clientSecret", "")
+	client := foursquarego.NewClient(httpClient, "foursquare", os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_SECRET"), "")
+	fmt.Println(client)
+
+	// Search Venues
+	venues, _, _ := client.Venues.Search(&foursquarego.VenueSearchParams{
+		LatLong:    "41.293209, -95.996140",
+		Radius:     2000,
+		CategoryID: []string{"4bf58dd8d48988d1ca941735"},
+		Intent:     foursquarego.IntentCheckin,
+	})
+	for _, loc := range venues {
+		fmt.Println(loc.Location)
+	}
 
 }
 
---
+++ go.mod
@@ -0,0 +1,7 @@
+module pizza
+
+require (
+	github.com/dghubble/sling v1.2.0 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/peppage/foursquarego v4.0.0+incompatible
+)
