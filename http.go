74d475bebe2c030c54b5ee7154bc6e472dcaa3eb
mattdsteele <orphum@gmail.com>
2019-02-21 22:26:46 -0600

    Add CORS header

--- http.go
+++ http.go
@@ -6,6 +6,16 @@ import (
 )
 
 func HttpHandler(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
 	q := r.URL.Query()
 	lat := q.Get("lat")
 	lon := q.Get("lon")
